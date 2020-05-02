7fc307300817e896883057ec1e5e635beb2c3819
Nick Harrison <harrison.nick.15@gmail.com>
2020-05-02 11:28:59 -0700

    push to have same code for everyone

--- assets/js/map_script.js
+++ assets/js/map_script.js
@@ -14,17 +14,23 @@ let map = tt.map({
 map.on('load', function () {
     map.setLanguage("en-US");
     let coordinates = { Br: [-0.009184, 51.51279], Sp: [-3.703790, 40.416775], Fr: [2.3488, 48.85341] };
-    let marker = new tt.Marker().setLngLat(coordinates.Sp).addTo(map);
+    let spainMarker = new tt.Marker().setLngLat(coordinates.Sp).addTo(map);
+    let britainMarker = new tt.Marker().setLngLat(coordinates.Br).addTo(map);
+    let franceMarker = new tt.Marker().setLngLat(coordinates.Fr).addTo(map);
     let popupOffsets = {
         top: [0, 0],
-        bottom: [0, -70],
+        bottom: [0, 0],
         'bottom-right': [0, -70],
         'bottom-left': [0, -70],
         left: [25, -35],
         right: [-25, -35]
     }
 
-    let popup = new tt.Popup({ offset: popupOffsets }).setHTML("Here is Madrid");
-    marker.setPopup(popup).togglePopup();
+    let spPopup = new tt.Popup({ offset: popupOffsets, className: "marker" }).setHTML("Here is Sapin");
+    let brPopup = new tt.Popup({ offset: popupOffsets, className: "marker" }).setHTML("Here is Britain");
+    let frPopup = new tt.Popup({ offset: popupOffsets, className: "marker" }).setHTML("Here is France");
+    spainMarker.setPopup(spPopup).togglePopup();
+    britainMarker.setPopup(brPopup).togglePopup();
+    franceMarker.setPopup(frPopup).togglePopup();
 });
 
