ea436281c1f7acff81dd2acf794dc14e020e0671
JMan <60026263+JMantis0@users.noreply.github.com>
2020-05-07 09:57:25 -0700

    stashing

--- assets/js/map_script.js
+++ assets/js/map_script.js
@@ -1,3 +1,6 @@
+// testing Maps api: https://developer.tomtom.com/
+// API key: UidCozvRt9CII6OELF37fl3Z07e2PyGh 
+// *2500 free transactions per day
 $(document).ready(function () {
 	
 	$("#translating").attr("style", "display: none")
@@ -163,3 +166,4 @@ $(document).ready(function () {
 	assignListeners();
  
  });
+
---
+++ assets/js/script-jesse.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+
+	$("#exampleModal1").foundation("open");
+
+});
